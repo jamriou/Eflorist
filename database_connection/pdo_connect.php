05fc68c5e57fef6cec71fd5ba2db3adb093c876b
James Leute <jamesleute@hotmail.com>
2023-09-12 13:07:03 -0400

    Delete img and database_connection directory

--- database_connection/pdo_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dsn = "mysql:server=localhost;Database=eflorist";
-$options = [
-  PDO::ATTR_EMULATE_PREPARES   => false, 
-  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
-  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, 
-];
-?>
