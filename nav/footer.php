023238b62cae205ff5b340290904edb01a062616
James Leute <jamesleute@hotmail.com>
2023-09-12 11:51:44 -0400

    Added translation to footer

--- nav/footer.php
+++ nav/footer.php
@@ -4,16 +4,21 @@
 ?>
 <div class="container footer-bottom clearfix">
       <div class="copyright">
-        <strong><span>eFlorist</span></strong>. All names, locations, and checkouts in this production are fictitious. No identification with actual places and products is intended or should be inferred.
-      </div>
+      <?php
+            if($_SESSION['language'] == "en"){?>
+                <strong><span>eFlorist</span></strong>. All names, locations, and checkouts in this production are fictitious. No identification with actual places and products is intended or should be inferred.
+            <?php }
+            if($_SESSION['language'] == "fr"){?>
+                <strong><span>eFlorist</span></strong>. Tous les noms, lieux et caisses de cette production sont fictifs. Aucune identification avec des lieux et des produits réels n’est prévue ou ne devrait être déduite.
+            <?php } ?>
       <div class="credits">
         <?php
             if($_SESSION['language'] == "en"){?>
-                <a class="title" href="scripts/language.php" >Fr - Français<a>
+                <a class="title" href="scripts/language.php" >Fr<a>
                 <img src="img/francais.jpg" width="20" height="20"></img>
             <?php }
             if($_SESSION['language'] == "fr"){?>
-                <a class="title" href="scripts/language.php" >En - English<a>
+                <a class="title" href="scripts/language.php" >En<a>
                 <img src="img/english.jpg" width="20" height="20"></img>
             <?php } ?>
       </div>
