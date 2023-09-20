e2cb4e85626e4ab6baffa361ddff208085a3f5d3
James Leute <jamesleute@hotmail.com>
2023-09-20 10:17:46 -0400

    Added profile.php and deleted old code

--- locales/contactUs_locales.php
+++ locales/contactUs_locales.php
@@ -3,39 +3,6 @@ $loginAgain = array(
     "en"=>"Oops, it's been too long, please log in again!",
     "fr"=>"Oops, ca fait trop longtemps, reconnecter vous!"
 );
-$h1Title = array(
-"en"=>" Elegant and beautiful plants",
-"fr"=>" Plantes magnifiques et charmantes"
-);
-$h1UnderTitle= array("en"=>"We are dedicated to bringing life and joy right at your doorstep","fr"=>"Nous nous engageons à apporter la vie et la joie à votre porte!");
-$h1Button= array(
-"en"=> "View best-sellers" , 
-"fr"=>"Voir meilleurs vendeurs"
-);
-$tropicals= array(
-    "en"=>"Tropicals",
-    "fr"=>"Tropicales"
-);
-$tropicalsDef= array(
-    "en"=>"View our most unique and distinct plants that can't wait to brighten up the indoors of your safe haven!",
-    "fr"=>"Découvrez nos plantes les plus uniques et distinctes pour égayer l'intérieur de votre havre de paix!"
-);
-$cacti= array(
-    "en"=>"Cacti", 
-    "fr"=>"Cactus"
-);
-$cactiDef= array(
-    "en"=>"From spiky to fluffy, view our wide range of cacti, one of these certainly won't be enough! Don't worry, they are really friendly!", 
-    "fr"=>"Du pointu au plus doux, regardez notre large gamme de cactus, l'un d'entre eux ne suffira certainement pas!"
-);
-$outdoors= array(
-    "en"=>"Garden", 
-    "fr"=>"Jardin"
-);
-$outdoorsDef= array(
-    "en"=>"Ready to spruce up your outdoors? View our plants best suited to stay outside and grow healthy crops!", 
-    "fr"=>"Prêt à embellir votre extérieur? Découvrez nos plantes les mieux adaptées pour rester à l'extérieur et faire pousser des cultures saines!"
-);
 $contact= array(
     "en"=>"Contact", 
     "fr"=>"Contacter"
@@ -59,13 +26,5 @@ $call= array(
 $findUs= array(
     "en"=>"You can find us here :", 
     "fr"=>"Vous pouvez nous trouver ici :"
-);
-$welcome= array(
-    "en"=>"Welcome", 
-    "fr"=>"Bienvenue"
-);
-$ready= array(
-    "en"=>"Ready to start shopping our best-sellers?", 
-    "fr"=>"Prêt à magasiner nos meilleur vendeurs?"
 );
 ?>
